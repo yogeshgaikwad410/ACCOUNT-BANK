e2f21827ff73359ee83370b8ede3e8f6100bdcda
yogeshgaikwad410 <yogeshgaikwad410@gmail.com>
2020-06-11 20:32:34 +0530

    All Done..

--- src/main/java/com/example/demo/domain/CurrentAccount.java
+++ src/main/java/com/example/demo/domain/CurrentAccount.java
@@ -1,30 +1,46 @@
 package com.example.demo.domain;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="CurrentTable")
 public class CurrentAccount {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
-	
-	//@SequenceGenerator(name="seq", initialValue=1001, allocationSize=5000)
-	//@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
 	private int accountNumber;
-	//private String accountType;
-	//private String accountHolder;
-	//private Date dateOpened;
-	//private int branchCode;
-	//private int ifscCode;
-	private BigDecimal accountBalance;
+	private double accountBalance;
+	private String fullName;
+	
+	
+	
+	
+		public CurrentAccount() {
+		super();
+			}
+		
+		
+		public CurrentAccount(Long id, double d, double accountBalance, String fullName) {
+			super();
+			this.id = id;
+			this.accountNumber = accountNumber;
+			this.accountBalance = accountBalance;
+			this.fullName = fullName;
+		}
+
+
+		public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -38,24 +54,11 @@ public class CurrentAccount {
 		this.accountNumber = accountNumber;
 	}
 
-	/*
-	 * public String getAccountType() { return accountType; } public void
-	 * setAccountType(String accountType) { this.accountType = accountType; } public
-	 * String getAccountHolder() { return accountHolder; } public void
-	 * setAccountHolder(String accountHolder) { this.accountHolder = accountHolder;
-	 * } public Date getDateOpened() { return dateOpened; } public void
-	 * setDateOpened(Date dateOpened) { this.dateOpened = dateOpened; } public int
-	 * getBranchCode() { return branchCode; } public void setBranchCode(int
-	 * branchCode) { this.branchCode = branchCode; } public int getIfscCode() {
-	 * return ifscCode; } public void setIfscCode(int ifscCode) { this.ifscCode =
-	 * ifscCode; }
-	 */public BigDecimal getAccountBalance() {
+	public double getAccountBalance() {
 		return accountBalance;
 	}
-	public void setAccountBalance(BigDecimal accountBalance) {
+	public void setAccountBalance(double accountBalance) {
 		this.accountBalance = accountBalance;
 	}
-	
-	
 	
 }
--- src/main/java/com/example/demo/service/impl/CurrentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CurrentServiceImpl.java
@@ -1,14 +1,10 @@
 package com.example.demo.service.impl;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.example.demo.dao.CurrentAccountDao;
 import com.example.demo.domain.CurrentAccount;
 
@@ -20,33 +16,23 @@ public class CurrentServiceImpl {
 	 @Autowired 
 	 private CurrentAccountDao currentAccountDao;
 	 
-	  
-
 	public CurrentAccount createCurrentAccount(CurrentAccount currentAccount) {
 		currentAccount.setAccountNumber(accountGen());
 		return currentAccountDao.save(currentAccount);
 	}
 	
+		
+	public Optional<CurrentAccount> findOneAccount(Long id)
+	{
+		return currentAccountDao.findById(id);
+	}
 	
+
 	public List<CurrentAccount> findAll(){
+		
 		return currentAccountDao.findAll();
 	}
-	
-	
-	public CurrentAccount findOne(Long id){
-		return currentAccountDao.getOne(id);
-	}
-	
-	
-	public CurrentAccount getByCurrentAccountId(Long id)
-	{
-		return currentAccountDao.getOne(id);
-	}
-	public CurrentAccount updateCurrentAccount(CurrentAccount currentAccount, Long id)
-	{
-		return currentAccountDao.save(currentAccount);
-	}
-	
+
 	public void  deleteAccountById(Long id)
 	{
 		currentAccountDao.deleteById(id);
@@ -57,36 +43,17 @@ public class CurrentServiceImpl {
 		currentAccountDao.deleteAll();
 	}
 	
-
 	
-	public CurrentAccount deposit(Double amount, CurrentAccount currentAccount)
+	public CurrentAccount updateAccount(Long id, CurrentAccount currentAccount)
 	{
-		
-		BigDecimal newBalance = (currentAccount.getAccountBalance().plus().add(new BigDecimal(amount)));
-		currentAccount.setAccountBalance(newBalance);
+		currentAccount.setId(id);
 		return currentAccountDao.save(currentAccount);
-	
+		
 	}
-	
-	/*
-	 * // MANDATORY: Transaction must be created before.
-	 * 
-	 * @Transactional(propagation = Propagation.MANDATORY ) public void
-	 * addAmount(Long id, double amount) CurrentAccount account = this.findById(id);
-	 * if (account == null) { throw new
-	 * BankTransactionException("Account not found " + id); } double newBalance =
-	 * account.getBalance() + amount; if (account.getBalance() + amount < 0) { throw
-	 * new BankTransactionException( "The money in the account '" + id +
-	 * "' is not enough (" + account.getBalance() + ")"); }
-	 * account.setBalance(newBalance);}
-	 */
-    
-	
+		
 	private int accountGen() { 
 		return ++nextAccountNumber; 
 	}
-	
-	
-	
+
 	
 }
